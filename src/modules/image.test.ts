bc069b25f7575be7a39931519cd3c92761b49cf5
Josua Frank <frank.josua@gmail.com>
2024-06-08 23:37:05 +0200

    small prettier fixes

--- src/modules/image.test.ts
+++ src/modules/image.test.ts
@@ -3,12 +3,14 @@ import { parseImage } from "./image";
 
 describe("parseImageTag", () => {
   it("should parse a complex image with host, path, tag and digest", async () => {
-    const image = "ghcr.io/immich-app/immich-server:v1.105.1@sha256:658b40420d7a39d6eb34c797cec8d36ff315f5adb168301aaf27dc4eafc8e228";
+    const image =
+      "ghcr.io/immich-app/immich-server:v1.105.1@sha256:658b40420d7a39d6eb34c797cec8d36ff315f5adb168301aaf27dc4eafc8e228";
     const expected = {
       host: "ghcr.io",
       path: "immich-app/immich-server",
       tag: "v1.105.1",
-      digest: "sha256:658b40420d7a39d6eb34c797cec8d36ff315f5adb168301aaf27dc4eafc8e228",
+      digest:
+        "sha256:658b40420d7a39d6eb34c797cec8d36ff315f5adb168301aaf27dc4eafc8e228",
     };
     expect(await parseImage(image)).toEqual(expected);
   });
